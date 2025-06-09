6b3894abaf1b6de946db053f7a4ef060468ce16a
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-06-09 10:43:34 +0200

    refactor: add foreign key constraints for comments and user relationships in migration files

---
+++ packages/backend/src/drizzle/migrations/0002_small_warlock.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "drizzle"."comments" ADD CONSTRAINT "comments_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "drizzle"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "drizzle"."comments" ADD CONSTRAINT "comments_post_id_posts_id_fk" FOREIGN KEY ("post_id") REFERENCES "drizzle"."posts"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "drizzle"."comments" ADD CONSTRAINT "comments_parent_comment_id_comments_id_fk" FOREIGN KEY ("parent_comment_id") REFERENCES "drizzle"."comments"("id") ON DELETE cascade ON UPDATE no action;
